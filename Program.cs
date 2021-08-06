3da6088a71dd9ce5d13b3b8ed2ee4be7b4c268e5
Austin Pierce <Austinentpierce@gmail.com>
2021-08-06 10:02:12 -0400

    finish exponent method

--- Program.cs
+++ Program.cs
@@ -2,11 +2,26 @@
 
 namespace ExponentMethod
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine(GetPow(3, 2));
+            Console.ReadLine();
+        }
+        static int GetPow(int baseNum, int powNum)
+        {
+            int result = 1;
+
+            for (int i = 0; i < powNum; i++)
+            {
+                result = result * baseNum;
+            }
+
+            return result;
+        }
+
     }
-  }
 }
